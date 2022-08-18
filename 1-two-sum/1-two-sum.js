9d6033a4408951d54bcb00d2dabdbc60cf346d7e
Siddharth Mal <39958179+sidmal11@users.noreply.github.com>
2022-08-18 09:16:47 -0400

    Time: 82 ms (86.96%), Space: 43.4 MB (22.78%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -1,12 +1,12 @@
 var twoSum = function(nums, target) {
-    let map = new Map();
-    
-    for(let i = 0; i < nums.length; i ++) {
-        if(map.has(target - nums[i])) {
-            return [map.get(target - nums[i]), i];
-        } else {
-            map.set(nums[i], i);
-        }
-    }
+	let hash = {};
+	
+	for(let i = 0; i < nums.length; i++) {
+		const n = nums[i];
+		if(hash[target - n] !== undefined) {
+			return [hash[target - n], i];
+		}
+		hash[n] = i;
+	}
 	return [];
-};
+}
