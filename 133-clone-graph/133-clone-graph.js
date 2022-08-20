38595f201829f39242b19c3b3f7be2c9eca2e9c8
Siddharth Mal <39958179+sidmal11@users.noreply.github.com>
2022-08-20 17:52:10 -0400

    Time: 294 ms (5.00%), Space: 51.2 MB (5.00%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    if (node === null) return node;
+    
+    const visited = new Map();
+    
+    const explore = (node) => {
+        let clone;
+        if (!visited.has(node.val)) {
+            clone = new Node(node.val);
+            visited.set(node.val,clone);
+            clone.neighbors = node.neighbors.map(explore)
+            console.log(node.val, visited);
+        }else{
+            clone = visited.get(node.val)
+        }
+        return clone;
+}
+    
+    return explore(node, visited)
+};
+
