93bb187fdafd7151a752e32e1a288ffb89922fb3
Siddharth Mal <39958179+sidmal11@users.noreply.github.com>
2023-02-05 11:14:38 -0500

    Time: 79 ms (38.92%), Space: 42.9 MB (63.60%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -9,20 +9,15 @@ var lastStoneWeight = function(stones) {
     stones.forEach(stone => {
         pq.enqueue(stone);
     })
+    
     while (pq.size() > 1){
         let first = pq.dequeue().element;
         let second = pq.dequeue().element;
-        console.log(first);
-        console.log(second);
-
-
         if (first == second){
             continue;
         }else{
             pq.enqueue(first-second);
         }
-        
-        console.log(pq.toArray())
     }
 
     return (pq.size()) ?  pq.front().element : 0;
